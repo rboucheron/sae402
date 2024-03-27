2ee5133e520661e47558f4e8cecc926ebb74ed27
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-03-27 15:24:45 +0100

    affichage de l'objet apres selection du bon pay

--- src/assets/ressources.jsx
+++ src/assets/ressources.jsx
@@ -1,6 +1,4 @@
 const IMAGES = {
     logo: new URL("./react.svg", import.meta.url).href,
-
- 
   };
   export default IMAGES; 
--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,9 @@
 import Maps from "./maps.jsx";
+
 function home() {
   return (
     <>
-      <h1 className="w-full text-center text-3xl text-blue mt-40">
-        Devine dans quelles pays se trouve la plus part des communautés amish ?{" "}
-      </h1>
+   
       <div className="mt-10 w-5/6 m-auto">
         <Maps />
       </div>
